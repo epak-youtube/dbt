5fc190c1a2a9535ab2187787312e6ff99c1bd3fa
Mike Adamski <m.adamski3@gmail.com>
2024-12-14 16:16:08 -0600

    use country code lookup in channel_basics__video

--- models/intermediate/int_channel_basics__video.sql
+++ models/intermediate/int_channel_basics__video.sql
@@ -13,17 +13,18 @@ with channel_basics__video as (
         cb.date as calendar_date,
         cb.live_or_on_demand,
         cb.subscribed_status,
-        cb.country_code,
+        cc.country_name,
         cb.view_count,
         cb.like_count,
         cb.dislike_count,
         cb.subscriber_gain_count,
         cb.subscribers_lost_count,
-        cb.watch_time_in_minutes,
+        cb.watch_time_in_minutes as total_watch_time_in_minutes,
         cb.avg_view_duration_in_seconds
     from {{ ref("stg_channel_basic") }} as cb
     left join {{ ref("stg_video") }} as v on cb.video_id = v.video_id
-    where cb.video_id is distinct from 'M8JBkd8KMJA'    -- has no match in the videos table; only has 2 views so just removing
+    left join {{ ref("country_codes_lookup") }} as cc on cc.country_code = cb.country_code
+    where cb.video_id is distinct from 'M8JBkd8KMJA'    -- has no match in the videos table and only 2 views so just removing
 )
 select
     *
