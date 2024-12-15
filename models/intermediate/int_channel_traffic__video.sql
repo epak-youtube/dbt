9f4343ed0d113a5f45ce20913b7b1ea77c677f8a
Mike Adamski <m.adamski3@gmail.com>
2024-12-14 22:59:37 -0600

    improvements to channel_traffic__video intermediate model

--- models/intermediate/int_channel_traffic__video.sql
+++ models/intermediate/int_channel_traffic__video.sql
@@ -1,20 +1,24 @@
 with channel_traffic__video as (
     select
         ct.*,
-        v.title as video_title,
+        v.video_title,
         v.published_at as video_published_at,
         datediff(day, video_published_at, calendar_date) as days_since_published,
-        case
-            when ct.traffic_source_raw = ct.channel_id then 'Organic-Channel'
-            when v_source.title is not null then 'Organic-Video'
-            else initcap(replace(ct.traffic_source_raw, '_', ' '))
-            end as traffic_source,
-        v_source.title as traffic_source_detail_video_title,
+        {# documentation on traffic sources: https://developers.google.com/youtube/reporting/v1/reports/dimensions#Traffic_Source_Dimensions #}
+        ts.traffic_source_name,
+        coalesce(
+            chan_source.channel_title,
+            case when ts.traffic_source_name = 'YouTube channels' and chan_source.channel_title is null then 'Other Channel' end,
+            v_source.video_title,
+            case when ts.traffic_source_name in ('Suggested videos', 'Interactive video endscreen', 'Video cards and annotations') and v_source.video_title is null then 'Other Video' end,
+            initcap(replace(ct.traffic_source_raw, '_', ' '))) as traffic_source_detail,
         cc.country_name
     from {{ ref("stg_channel_traffic") }} as ct
     left join {{ ref("stg_video") }} as v on ct.video_id = v.video_id
     left join {{ ref("country_codes_lookup") }} as cc on cc.country_code = ct.country_code
+    left join {{ ref("traffic_source_lookup") }} as ts on ts.traffic_source_id = ct.traffic_source_id
     left join {{ ref("stg_video") }} as v_source on ct.traffic_source_raw = v_source.video_id
+    left join {{ ref("stg_channel") }} as chan_source on ct.traffic_source_raw = chan_source.channel_id
     where ct.video_id is distinct from 'M8JBkd8KMJA'    -- has no match in the videos table and only 2 views so just removing
 )
 select
@@ -27,8 +31,8 @@ select
     live_or_on_demand,
     subscribed_status,
     traffic_source_id,
-    traffic_source,
-    traffic_source_detail_video_title,
+    traffic_source_name,
+    traffic_source_detail,
     country_code,
     country_name,
     view_count,
@@ -37,6 +41,3 @@ select
     red_views,
     red_watch_time_in_minutes
 from channel_traffic__video
-
-
-
