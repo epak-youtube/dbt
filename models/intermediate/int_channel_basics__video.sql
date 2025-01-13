dc5d7a2845bf8ec413db7cd73100660482732620
Mike Adamski <m.adamski3@gmail.com>
2025-01-12 21:44:24 -0600

    create intermediate video model and reference it in other intermediate tables that join to video data

--- models/intermediate/int_channel_basics__video.sql
+++ models/intermediate/int_channel_basics__video.sql
@@ -4,6 +4,7 @@ with channel_basics__video as (
         cb.video_id,
         v.video_title,
         v.published_at as video_published_at,
+        v.category_name as video_category,
         cb.calendar_date,
         datediff(day, video_published_at, calendar_date) as days_since_published,
         cb.live_or_on_demand,
@@ -15,13 +16,32 @@ with channel_basics__video as (
         cb.dislike_count,
         cb.subscriber_gain_count,
         cb.subscribers_lost_count,
-        cb.watch_time_in_minutes as total_watch_time_in_minutes,
-        cb.avg_view_duration_in_seconds
+        cb.total_watch_time_in_minutes,
+        cb.avg_view_duration_in_seconds,
+        cb._fivetran_synced
     from {{ ref("stg_channel_basic") }} as cb
-    left join {{ ref("stg_video") }} as v on cb.video_id = v.video_id
+    left join {{ ref("int_video") }} as v on cb.video_id = v.video_id
     left join {{ ref("country_codes_lookup") }} as cc on cc.country_code = cb.country_code
     where cb.video_id is distinct from 'M8JBkd8KMJA'    -- has no match in the videos table and only 2 views so just removing
 )
 select
-    *
+    channel_id,
+    video_id,
+    video_title,
+    video_published_at,
+    video_category,
+    calendar_date,
+    days_since_published,
+    live_or_on_demand,
+    subscribed_status,
+    country_code,
+    country_name,
+    total_view_count,
+    like_count,
+    dislike_count,
+    subscriber_gain_count,
+    subscribers_lost_count,
+    total_watch_time_in_minutes,
+    avg_view_duration_in_seconds,
+    _fivetran_synced
 from channel_basics__video
--- models/intermediate/int_channel_traffic__video.sql
+++ models/intermediate/int_channel_traffic__video.sql
@@ -3,18 +3,19 @@ with channel_traffic__video as (
         ct.*,
         v.video_title,
         v.published_at as video_published_at,
+        v.category_name as video_category,
         datediff(day, video_published_at, calendar_date) as days_since_published,
         {# documentation on traffic sources: https://developers.google.com/youtube/reporting/v1/reports/dimensions#Traffic_Source_Dimensions #}
         ts.traffic_source_name,
         coalesce(
-            chan_source.channel_title,
-            case when ts.traffic_source_name = 'YouTube channels' and chan_source.channel_title is null then 'Other Channel' end,
+            chan_source.title,
+            case when ts.traffic_source_name = 'YouTube channels' and chan_source.title is null then 'Other Channel' end,
             v_source.video_title,
             case when ts.traffic_source_name in ('Suggested videos', 'Interactive video endscreen', 'Video cards and annotations') and v_source.video_title is null then 'Other Video' end,
             initcap(replace(ct.traffic_source_raw, '_', ' '))) as traffic_source_detail,
         cc.country_name
     from {{ ref("stg_channel_traffic") }} as ct
-    left join {{ ref("stg_video") }} as v on ct.video_id = v.video_id
+    left join {{ ref("int_video") }} as v on ct.video_id = v.video_id
     left join {{ ref("country_codes_lookup") }} as cc on cc.country_code = ct.country_code
     left join {{ ref("traffic_source_lookup") }} as ts on ts.traffic_source_id = ct.traffic_source_id
     left join {{ ref("stg_video") }} as v_source on ct.traffic_source_raw = v_source.video_id
@@ -25,6 +26,7 @@ select
     channel_id,
     video_id,
     video_title,
+    video_category,
     video_published_at,
     days_since_published,
     calendar_date,
@@ -37,7 +39,12 @@ select
     country_name,
     view_count,
     watch_time_in_minutes,
-    avg_view_duration_in_seconds,
-    red_views,
-    red_watch_time_in_minutes
+    average_view_duration_in_seconds,
+    red_view_count,
+    red_watch_time_in_minutes,
+    _fivetran_synced
 from channel_traffic__video
+where true
+{% if is_incremental() %}
+  and cb._fivetran_synced > coalesce((select max(_fivetran_synced) from {{ this }}), '1900-01-01')
+{% endif %}
