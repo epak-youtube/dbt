bfddd84132752a08fa02233f74a900139ac784ad
Mike Adamski <m.adamski3@gmail.com>
2025-01-28 19:55:48 -0600

    create macro to generate a unique key for tables that don't have one

---
+++ macros/build_unique_key.sql
@@ -0,0 +1,9 @@
+{% macro build_unique_key(columns, delimiter='|', null_value='null_value') %}
+    md5(
+        '{{ this.identifier }}' ||
+        {% for column in columns %}
+            {{ 'coalesce(' ~ column ~ ', \'' ~ null_value ~ '\')' }}
+            {%- if not loop.last -%} || {% endif %}
+        {% endfor %}
+    )
+{% endmacro %}
