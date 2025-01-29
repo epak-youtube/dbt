f7ed42066f40712b721966979fd11b5a58540e5f
Mike Adamski <m.adamski3@gmail.com>
2025-01-28 20:08:59 -0600

    implement id field as new unique key for stg_audience_retention

--- models/staging/stg_audience_retention.sql
+++ models/staging/stg_audience_retention.sql
@@ -8,5 +8,10 @@ select
     elapsed_video_time_ratio::dec(18, 2) as percent_of_video_elapsed,
     audience_watch_ratio::dec(18, 4) as percent_watch_ratio,
     relative_retention_performance::dec(18, 4) as relative_retention_performance,
-    _fivetran_synced
+    _fivetran_synced,
+    {{ build_unique_key([
+        'video_id',
+        'calendar_date',
+        'percent_of_video_elapsed'
+        ]) }} as id
 from audience_retention
